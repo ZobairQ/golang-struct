d0a9c785c3b5f2f4d1357dcee417f9a5828ce85b
Zobair Qauomi <Zobairq55@gmail.com>
2021-01-10 15:50:13 +0100

    add new map, add entry in map and delete entry from map

--- maps.go
+++ maps.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 
 func newMap() {
+	var words map[string]string // declaration
+	words = map[string]string{} // new map()
+
+	wordsCreatedWithMake := make(map[string]string) // new map
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"blue":  "#0000FF",
 		"green": "#00FF00",
 	}
-	
+
+	words["go"] = "A programming language by Google"
+	wordsCreatedWithMake["go"] = "A programming language by Google"
+
+	delete(words, "go") // deletes by key
+
 	fmt.Println(colors)
+	fmt.Println(words)
+	fmt.Println(wordsCreatedWithMake)
 }
