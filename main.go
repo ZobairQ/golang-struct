40115c9db94f893e465ea640cb1d53608c9c0b99
Zobair Qauomi <Zobairq55@gmail.com>
2021-01-10 13:05:43 +0100

    add another struct inside person struct

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	john := person{firstName: "John", lastName: "Doe"}
+	john := person{
+		firstName: "John",
+		lastName:  "Doe",
+		contact: contactInfo{
+			email:   "doeJohn@bigCorp.com",
+			zipCode: 9400,
+		},
+	}
 	var peter person
+	var contact contactInfo
+
 	peter.firstName = "Peter"
 	peter.lastName = "Parker"
+
+	contact.email = "PeterParker@spiderman.com"
+	contact.zipCode = 1000
+
+	peter.contact = contact
+
 	fmt.Println(john)
 	fmt.Printf("%+v", peter) // Prints the keys and values
 }
